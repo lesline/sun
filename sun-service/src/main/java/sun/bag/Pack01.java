6f509c159f3da56794319aacb42830281138f45b
lesline <longzaishaolin861021@126.com>
2017-12-07 08:49:29 +0800

    bagpack

--- sun-service/src/main/java/sun/bag/Pack01.java
+++ sun-service/src/main/java/sun/bag/Pack01.java
@@ -1,5 +1,7 @@
 package sun.bag;
 
+import java.util.Arrays;
+
 public class Pack01 {
 
     public static void main(String[] args) {
@@ -7,19 +9,42 @@ public class Pack01 {
 
         int c[] = {3, 5, 2, 7, 4};
         int v[] = {2, 4, 1, 6, 5};
-        int f[][] = new int[6][10];
+        int f[][] = new int[6][11];
 
+   /*  1 2 3 4 5 6 7 8 9 10
+   3 2 0 0 2 2 2 2 2 2 2 2
+   5 4 0 0 2 2 4 4 4 6 6 6
+   2 1
+   7 6
+   4 5
+    */
+        for (int i = 0; i < 5; i++) {
+            System.out.println();
+            System.out.print("『"+i+"』"+c[i] + " " + v[i] + "--->");
 
-        for (int i = 1; i < 6; i++)
-            for (int j = 1; j < 10; j++) {
-                System.out.println(i+"-"+j);
+            for (int j = 1; j < 11; j++) {
+                if(i==0){
+                    f[0][j]=j>v[0]?v[0]:0;
+                    System.out.print(f[i][j] +" ");
+                    continue;
+                }
                 if (c[i] > j)//如果背包的容量，放不下c[i]，则不选c[i]
                     f[i][j] = f[i - 1][j];
                 else {
                     f[i][j] = getMaxx(f[i - 1][j], f[i - 1][j - c[i]] + v[i]);//转移方程式
                 }
+                System.out.print(f[i][j] +" ");
             }
-        System.out.println(f);
+        }
+
+        for (int i = 0; i < f.length; i++) {
+            System.out.println();
+            for (int j = 0; j < f[i].length; j++) {
+                System.out.print(f[i][j]);
+
+            }
+
+        }
     }
 
     private static int getMaxx(int a, int b) {
