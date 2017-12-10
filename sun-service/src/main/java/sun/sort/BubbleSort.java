957f1015e1eda380aec29dec7921ab06abe798e0
lesline <longzaishaolin861021@126.com>
2017-12-10 22:50:23 +0800

    bagpack

--- sun-service/src/main/java/sun/sort/BubbleSort.java
+++ sun-service/src/main/java/sun/sort/BubbleSort.java
@@ -16,14 +16,20 @@ public class BubbleSort {
 
     public static void sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
+            boolean didSwap = false;//优化后最好时间复杂度为n,不优化为n2
+
             for (int j = 0; j < array.length-i-1; j++) {
 
                     if(array[j]<array[j+1]){
                         int tmp=array[j];
                         array[j]=array[j+1];
                         array[j+1]=tmp;
-                    }
+                        didSwap=true;
 
+                    }
+            }
+            if(didSwap==false){
+                return;
             }
 
         }
--- sun-service/src/main/java/sun/sort/readme.md
+++ sun-service/src/main/java/sun/sort/readme.md
@@ -3,11 +3,11 @@
                     冒泡排序    合并排序    快速排序    二分查找
 最坏时间复杂度          n2       nlog2n        n2
 
-最好时间复杂度          n2       nlog2n       nlog2n
+最好时间复杂度          n2/n     nlog2n       nlog2n
 
-平均时间复杂度          n2       nlog2n      nlog2n
+平均时间复杂度          n2       nlog2n       nlog2n
 
-空间复杂度                                   log2n
+空间复杂度              1                     log2n
 
 
 选择、插入：n2
